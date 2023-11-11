c8a5580a6e096bcb9e19896ef71e0ac4094493ee
li3zhen1 <master_lee@live.com>
2023-11-11 16:40:10 -0500

    Update glyphs

--- assets/test-code.cpp
+++ assets/test-code.cpp
@@ -1,3 +1,4 @@
+#include <cstdio>
 #include <iostream>
 #include <string>
 #include <vector>
@@ -5,7 +6,7 @@
 using namespace std;
 
 class Typeface {
-public:
+  public:
     enum TypefaceStyle {
         Monospaced = 0x0,
         Proportional = 0x1,
@@ -14,18 +15,19 @@ public:
 
 int main() {
 
-    const auto style = Typeface::Monospaced;
-
-    const auto specimenText = new vector<string> {
-        "sphinx of black quartz, judge my vow.\n",
+    const auto specimenText = new vector<string>{
+        "sphinx of black quartz, judge my vow.",
+        "the quick brown fox jumps over the lazy dog."
     };
-    
-    specimenText->push_back(
-        "the quick brown fox jumps over the lazy dog.\n"
-    );
 
-    // print a sentence that contains all lower-case letters.
-    fprintf(stdout, "%u", style);
+    specimenText->push_back({
+        "SPHINX OF BLACK QUARTZ, JUDGE MY VOW.",
+        "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG."
+    });
+
+    for (uint i = 0; i != specimenText->size(); ++i) {
+        fprintf(stdout, "%s\n", (*specimenText)[i].c_str());
+    }
 
     return 0;
 }
